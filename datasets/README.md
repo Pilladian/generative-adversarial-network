8b3aca622d697d1c9291a5fca6dd7ddf6ed69411
pz <philipp.zimmermann@t-systems.com>
2022-05-02 14:50:00 +0200

    MNIST dataset added

---
+++ datasets/README.md
@@ -0,0 +1,4 @@
+# Dataset Overview
+
+## MNIST
+Contains images of numbers between 0 and 9
---
+++ datasets/__init__.py
@@ -0,0 +1,38 @@
+# Python 3.8.10
+
+from torch.utils.data import Dataset
+import os
+from PIL import Image
+import torch
+
+
+class MNIST(Dataset):
+
+    def __init__(self, root, train=False, eval=False, test=False, transform=None):
+        self.root = root
+        self.data_loc = f"{self.root}{'train/' if train else 'eval/' if eval else 'test/' if test else ''}"
+        self.transform = transform
+        self.data = self.get_data()
+        
+    def get_data(self):
+        d = {"image_file": [], "label": []}
+
+        for i in os.listdir(self.data_loc):
+            l = i.split('_')
+            d["image_file"].append(i)
+            d["label"].append(l[0])
+
+        return d
+
+    def __len__(self):
+        return len(self.data['image_file'])
+
+    def __getitem__(self, idx):
+        img_loc = self.data["image_file"][idx]
+        image = Image.open(os.path.join(self.data_loc, img_loc))
+        label = torch.tensor(float(self.data["label"][idx]))
+
+        if self.transform is not None:
+            image = self.transform(image)
+
+        return (image, label)
